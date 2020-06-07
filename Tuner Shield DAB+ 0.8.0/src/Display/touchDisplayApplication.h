7bb80a7da868da51433915de26087be1679c5025
IGBTechmember <igb@posteo.de>
2020-06-07 08:43:49 +0200

    Removed Touch Display
    Updated library porperties
    Removed Tocuh Display

--- Tuner Shield DAB+ 0.8.0/src/Display/touchDisplayApplication.h
+++
@@ -1,8 +0,0 @@
-/*Touch display application*/
-/*include guard*/
-#ifndef TOUCH_DISPLAY_APPLICATION_H
-#define TOUCH_DISPLAY_APPLICATION_H
-
-bool callTouchDisplayApplication();
-
-#endif //TOUCH_DISPLAY_APPLICATION_H
